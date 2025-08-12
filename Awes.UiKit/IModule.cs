52d3f178fdbcb4f760ffc5680e3fc754500ed3f4
jakob <aajakobsung@gmail.com>
2025-08-12 22:59:18 +0900

    초기 커밋.

---
+++ Awes.UiKit/IModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Awes.UiKit
+{
+    internal interface IModule
+    {
+        string Name { get; set; }
+
+    }
+}
