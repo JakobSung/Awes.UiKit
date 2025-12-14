c8c3b2cdde268bece1f4053bba35e36512d5f2bf
jakob <aajakobsung@gmail.com>
2025-12-14 23:22:19 +0900

    uikit.wpf implement generic host

--- Awes.UiKit.Wpf.Sample/App.xaml.cs
+++ Awes.UiKit.Wpf.Sample/App.xaml.cs
@@ -1,28 +1,36 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace Awes.UiKit.Wpf.Sample
 {
-    
-
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : AwesWpfApp
+    public partial class App : Application
     {
-        //private WPFHost _host;
-
         public App()
         {
-            
+            WpfHost host = new WPFHostBuilder(this)
+                                    .ConfigureStartWindow<MainWindow>()
+                                    .ConfigureServices(services =>
+                                    {
+                                        //todo: add services here
+                                    })
+                                    .Build();
+
+            Task.Run(async () => await host.RunAsync());
         }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            
         }
+
+
+        
+        
 
 
     }
--- Awes.UiKit.Wpf/WPFHostBuilder.cs
+++ Awes.UiKit.Wpf/WPFHostBuilder.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -9,44 +9,95 @@ namespace Awes.UiKit.Wpf
 {
     public class WPFHostBuilder
     {
-        Application _app;
+        private Application? _app = null;
+        private WpfHost _host = new WpfHost();
+        private Window? _mainWindow = null;
+        private ServiceCollection _services = new ServiceCollection();
+        private object? _mainVm = null;
+
+        private Type _mainWindowType = null;
 
         public WPFHostBuilder(Application app)
         {
             _app = app;
-
-            //app.StartupUri = null;
-
             _app.Startup += App_Startup;
             _app.Exit += App_Exit;
+
+            _host = new WpfHost();
         }
+
+        public IServiceCollection Services => _services;
 
         private void App_Exit(object sender, ExitEventArgs e)
         {
-            
+            // Trigger graceful stop
+            _ = _host.StopAsync();
         }
 
         private void App_Startup(object sender, StartupEventArgs e)
         {
-            
+            if (_mainWindowType == null)
+            {
+                throw new InvalidOperationException("MainWindow is not configured. Please call ConfigureStartWindow<T> to configure the main window.");
+            }
+
+            _mainWindow ??= (Window)_host.Services.GetRequiredService(_mainWindowType);
+            _mainWindow.DataContext = _mainVm;
+            _mainWindow.Show();
         }
 
-        //public object ConfigureStartWindow<T>()
-        //{
-            
-        //}
-
-        public object ConfigureServices(System.Action<Microsoft.Extensions.DependencyInjection.IServiceCollection> action)
+        public virtual WPFHostBuilder ConfigureStartWindow<T>(object? mainVm = null) where T : Window
         {
-            action.Invoke(_builder.Services);
+            _mainWindowType = typeof(T);
+            _services.AddTransient<T>();
+            _mainVm = mainVm;
+            return this;
         }
 
-        //public IHost Build()
-        //{
-        //    var host = _builder.Build();
-        //    AwesUiKit.RegistServiceProvdier(host.Services);
-        //    return host;
-        //}
+        public WPFHostBuilder ConfigureServices(Action<IServiceCollection> action)
+        {
+            action.Invoke(this.Services);
+            return this;
+        }
+
+        public WpfHost Build()
+        {
+            var serviceProvider = _services.BuildServiceProvider();
+            _host.Initialize(serviceProvider);
+            return _host;
+        }
     }
 
+    public class WpfHost : IHost
+    {
+        private IServiceProvider _serviceProvider;
+        private bool _started;
+
+        public IServiceProvider Services => _serviceProvider ?? throw new InvalidOperationException("Host not initialized. Call Initialize before use.");
+
+        internal void Initialize(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Dispose()
+        {
+            if (_serviceProvider is IDisposable d)
+            {
+                d.Dispose();
+            }
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            _started = true;
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken = default)
+        {
+            _started = false;
+            return Task.CompletedTask;
+        }
+    }
 }
